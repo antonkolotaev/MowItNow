28cc2e55eb3f2fce1523180a3e1a43493bee2bbd
Anton Kolotaev <anton.kolotaev@gmail.com>
2016-09-28 21:45:47 +0200

    LawnMower is independent on lawn

--- src/main/scala/mowitnow/LawnMower.scala
+++ src/main/scala/mowitnow/LawnMower.scala
@@ -1,22 +1,23 @@
 package mowitnow
 
-case class LawnMower(isMoveAllowed : (Position, Position) => Boolean, position: Position, orientation: Orientation)
+case class LawnMower(position: Position, orientation: Orientation)
 {
     def left  = copy(orientation = orientation.left)
     def right = copy(orientation = orientation.right)
-    def forward = copy(position = {
-        val newPosition = position treat orientation
-        if (isMoveAllowed (position, newPosition))
-            newPosition
-        else
-            position
-    })
+    def forward(isMoveAllowed : (Position, Position) => Boolean) =
+        copy(position = {
+            val newPosition = position treat orientation
+            if (isMoveAllowed (position, newPosition))
+                newPosition
+            else
+                position
+        })
 
-    def handle(command : Command) =
+    def handle(command : Command, isMoveAllowed : (Position, Position) => Boolean) =
         command match {
             case Left    => left
             case Right   => right
-            case Forward => forward
+            case Forward => forward(isMoveAllowed)
         }
 }
 
--- src/main/scala/mowitnow/Player.scala
+++ src/main/scala/mowitnow/Player.scala
@@ -17,7 +17,9 @@ object Player
             case Some(outlier) => Failure(OutOfLawn(outlier.position))
             case None =>
                 Success(mowers map { mower =>
-                    mower.commands.foldLeft(LawnMower(lawn.isMoveAllowed, mower.position, mower.orientation)) { _ handle _ }
+                    mower.commands.foldLeft(LawnMower(mower.position, mower.orientation)) {
+                        (mower, command) => mower handle (command, lawn.isMoveAllowed)
+                    }
                 } map { mower =>
                     (mower.position, mower.orientation)
                 })
@@ -41,31 +43,55 @@ object Player
 
                 } map { positions =>
 
-                        mowers.foldLeft((positions, Set.empty[Position], List.empty[(Position, Orientation)])) {
+                        mowers.foldLeft((positions, List.empty[(Position, Orientation)])) {
 
-                            case ((unprocessedPositions, processedPositions, processedMowers), mower) =>
+                            case ((ps, processedMowers), mower) =>
 
-                                val unprocessedPositionsWithoutMe = unprocessedPositions - mower.position
+                                val positionsWithoutMe = ps - mower.position
 
                                 def positionAllowed(position: Position, newPosition : Position) = {
-                                    lawn.isMoveAllowed(position, newPosition) &&
-                                        !(unprocessedPositionsWithoutMe contains newPosition) &&
-                                        !(processedPositions contains newPosition)
+                                    lawn.isMoveAllowed(position, newPosition) && !(positionsWithoutMe contains newPosition)
                                 }
 
-                                val newMower = mower.commands.foldLeft(LawnMower(positionAllowed, mower.position, mower.orientation)) {
-                                    _ handle _
+                                val newMower = mower.commands.foldLeft(LawnMower(mower.position, mower.orientation)) {
+                                    (mower, command) => mower handle (command, positionAllowed)
                                 }
 
-                                (unprocessedPositionsWithoutMe,
-                                    processedPositions + newMower.position,
+                                (positionsWithoutMe + newMower.position,
                                     (newMower.position, newMower.orientation) :: processedMowers)
 
-                        }._3.reverse
+                        }._2.reverse
                 }
         }
 
 
+    }
+
+    def concurrent(lawn: Lawn, mowers : Seq[Def]) : Seq[(Position, Orientation)] =
+    {
+        mowers map { mower => (mower.position, mower.orientation) }
+    }
+
+
+    def concurrent(upperRight : Position, mowers : Seq[Def]) : Try[Seq[(Position, Orientation)]] =
+    {
+        val lawn = new Lawn(upperRight.x + 1, upperRight.y + 1)
+
+        mowers find (m => !(lawn contains m.position)) match {
+            case Some(outlier) => Failure(OutOfLawn(outlier.position))
+            case None =>
+                mowers.foldLeft(Success(Set.empty[Position]): Try[Set[Position]]) {
+                    case (Success(ps), m) =>
+                        if (ps contains m.position)
+                            Failure(DuplicatePosition(m.position))
+                        else
+                            Success(ps + m.position)
+                    case (f@Failure(_), _) => f
+
+                } map { positions =>
+                    concurrent(lawn, mowers)
+                }
+        }
     }
 
 }
